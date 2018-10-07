51f6d794f65dff19950ed60e22d8ee6ee99897a5
Riyakhazanchi <33264154+Riyakhazanchi@users.noreply.github.com>
2018-10-07 19:40:03 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -78,12 +78,16 @@ app.use(passport.session());
 
  ////connect flash
  app.use(flash());
+  
+ app.use(function(req,res,next){
+  res.locals.success_msg=req.flash('success_msg');
+  res.locals.error_msg=req.flash('error_msg');
+  res.locals.error=req.flash('error');
+  res.locals.user=req.user||null;
+  next();
+ })
 
-//global var
- 
-
-
-app.use('/',index);
+app.use('/',index); 
 app.use('/users',users);
 
 
